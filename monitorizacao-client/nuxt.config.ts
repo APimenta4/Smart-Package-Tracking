27822ce3817887cf344a4c7a649d8ce67382e9ff
APimenta4 <2223048@my.ipleiria.pt>
2024-12-27 10:45:34 +0000

    page created

---
+++ monitorizacao-client/nuxt.config.ts
@@ -0,0 +1,25 @@
+import Aura from "@primevue/themes/aura";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  modules: ["@primevue/nuxt-module"],
+  primevue: {
+    options: {
+      theme: {
+        preset: Aura,
+        options: {
+          darkModeSelector: ".my-app-dark",
+        },
+      },
+    },
+  },
+});
