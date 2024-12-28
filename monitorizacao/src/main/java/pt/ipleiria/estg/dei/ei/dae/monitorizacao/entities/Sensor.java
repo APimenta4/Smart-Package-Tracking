e5b7c9baf10a871bef0fa554e282a6886456204e
Patinhooh <119509409+patinhooh@users.noreply.github.com>
2024-12-28 22:27:14 +0000

    feat: Add entities Sensor and Volume (only the necessary for Sensor)

--- monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Sensor.java
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Sensor.java
@@ -1,4 +1,76 @@
 package pt.ipleiria.estg.dei.ei.dae.monitorizacao.entities;
 
-public class Sensor {
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import pt.ipleiria.estg.dei.ei.dae.monitorizacao.enums.SensorType;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "sensors")
+@NamedQueries({
+        @NamedQuery(
+                name = "getAllSensors",
+                query = "SELECT s FROM Sensor s"
+        )
+})
+public class Sensor implements Serializable {
+    @Id
+    private long code;
+
+    @ManyToOne
+    @NotNull
+    private Volume volume;
+
+    @NotNull
+    private SensorType type;
+
+
+    public Sensor(long code, Volume volume, SensorType type) {
+        this.code = code;
+        this.volume = volume;
+        this.type = type;
+    }
+
+    public Sensor() {
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public @NotNull Volume getVolume() {
+        return volume;
+    }
+
+    public void setVolume(@NotNull Volume volume) {
+        this.volume = volume;
+    }
+
+    public @NotNull SensorType getType() {
+        return type;
+    }
+
+    public void setType(@NotNull SensorType type) {
+        this.type = type;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Sensor sensor = (Sensor) obj;
+        return code == sensor.code;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code);
+    }
 }
--- monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Volume.java
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Volume.java
@@ -1,4 +1,35 @@
 package pt.ipleiria.estg.dei.ei.dae.monitorizacao.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+
+import java.util.List;
+
+@Entity
 public class Volume {
+    @Id
+    private long code;
+
+    @OneToMany(mappedBy = "sensor")
+    private List<Sensor> sensors;
+
+    public Volume() {
+    }
+
+    public long getCode() {
+        return code;
+    }
+
+    public void setCode(long code) {
+        this.code = code;
+    }
+
+    public List<Sensor> getSensors() {
+        return sensors;
+    }
+
+    public void setSensors(List<Sensor> sensors) {
+        this.sensors = sensors;
+    }
 }
