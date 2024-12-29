259d434935b8cd992b69307d8a63a88008147f3f
Patinhooh <119509409+patinhooh@users.noreply.github.com>
2024-12-29 22:33:43 +0000

    feat: Volume entity

--- monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Volume.java
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/entities/Volume.java
@@ -6,7 +6,11 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import pt.ipleiria.estg.dei.ei.dae.monitorizacao.enums.PackingType;
+import pt.ipleiria.estg.dei.ei.dae.monitorizacao.enums.VolumeStatus;
 
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Entity
@@ -18,11 +22,27 @@ public class Volume {
     @NotNull
     private Order order;
 
+    private VolumeStatus status;
+
+    private PackingType packingType;
+
+    private Date shippedDate;
+
+    private Date deliveredDate;
+
     @OneToMany(mappedBy = "sensor")
     @NotEmpty
     private List<Sensor> sensors;
 
+    public Volume(Order order, VolumeStatus status, PackingType packingType) {
+        this.order = order;
+        this.status = status;
+        this.packingType = packingType;
+        this.sensors = new ArrayList<>();
+    }
+
     public Volume() {
+        this.sensors = new ArrayList<>();
     }
 
     public long getCode() {
---
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/enums/VolumeStatus.java
@@ -0,0 +1,9 @@
+package pt.ipleiria.estg.dei.ei.dae.monitorizacao.enums;
+
+public enum VolumeStatus {
+    READY_FOR_PICKUP,
+    IN_TRANSIT,
+    DELIVERED,
+    RETURNED,
+    CANCELLED
+}
