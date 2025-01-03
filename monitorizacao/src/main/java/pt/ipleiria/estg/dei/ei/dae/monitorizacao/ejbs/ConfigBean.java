6ac5f06668c9c97ed12d8429b3b21dac8717bd9f
patinhooh <119509409+patinhooh@users.noreply.github.com>
2025-01-03 16:12:19 +0000

    feat: Creating users on populate DB

--- monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/ejbs/ConfigBean.java
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/ejbs/ConfigBean.java
@@ -12,26 +12,27 @@ import java.util.logging.Logger;
 public class ConfigBean {
     @EJB
     private ClientBean clientBean;
+    @EJB
+    private LogisticianBean logisticianBean;
+    @EJB
+    private ManagerBean managerBean;
 
     private static final Logger logger = Logger.getLogger("ejbs.ConfigBean");
 
     @PostConstruct
     public void populateDB()
     {
-        System.out.println("Hello Java EE!");
+        logger.info("Starting to populate DB");
 
-
-
-        // create clients
-        logger.info("Creating clients ...");
+        // create users
+        logger.info("Creating users");
         try {
-            clientBean.create(1L,"manuel", "manuel@mail.pt","123");
-            clientBean.create(2L,"joao", "joao@mail.pt","123");
-            clientBean.create(3L,"maria", "maria@mail.pt","123");
+            clientBean.create(1L,"client", "client@mail.pt","123");
+            logisticianBean.create(2L,"logistician", "logistician@mail.pt","123");
+            managerBean.create(3L,"manager", "manager@mail.pt","123");
+            logger.info("Users created");
         } catch (Exception e) {
             logger.severe(e.getMessage());
         }
-
-
     }
 }
