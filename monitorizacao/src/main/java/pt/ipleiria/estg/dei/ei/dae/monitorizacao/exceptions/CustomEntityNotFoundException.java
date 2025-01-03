e0ecda698dcd83b88c414ce5de34b75380b9c48f
patinhooh <119509409+patinhooh@users.noreply.github.com>
2025-01-03 17:02:30 +0000

    feat: logging on creation of a exception

--- monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/exceptions/CustomEntityNotFoundException.java
+++ monitorizacao/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitorizacao/exceptions/CustomEntityNotFoundException.java
@@ -1,6 +1,10 @@
 package pt.ipleiria.estg.dei.ei.dae.monitorizacao.exceptions;
 
+import java.util.logging.Logger;
+
 public class CustomEntityNotFoundException extends Exception {
+    // TODO: logger is also in the wrapper
+    private static final Logger logger = Logger.getLogger("exceptions.CustomEntityNotFoundException");
     /**
      * The message will be appended with " not found".
      *
@@ -8,5 +12,6 @@ public class CustomEntityNotFoundException extends Exception {
      */
     public CustomEntityNotFoundException(String message) {
         super(message + " not found");
+        logger.warning("ERROR: " + getMessage());
     }
 }
