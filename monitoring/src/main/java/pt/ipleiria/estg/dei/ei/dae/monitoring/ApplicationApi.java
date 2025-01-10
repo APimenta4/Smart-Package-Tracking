de67a6d84867b4100d91518b3d607a9e431b00a5
patinhooh <119509409+patinhooh@users.noreply.github.com>
2025-01-10 00:09:37 +0000

    fix: url

--- monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/ApplicationApi.java
+++ monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/ApplicationApi.java
@@ -3,7 +3,7 @@ package pt.ipleiria.estg.dei.ei.dae.monitoring;
 import jakarta.ws.rs.ApplicationPath;
 import jakarta.ws.rs.core.Application;
 
-@ApplicationPath("/monitoring")
-public class ApplicationMonitoring extends Application {
+@ApplicationPath("/api")
+public class ApplicationApi extends Application {
 
 }
