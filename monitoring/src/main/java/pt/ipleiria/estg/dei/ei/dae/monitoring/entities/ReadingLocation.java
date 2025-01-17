178a8f7228910f299d70d3d037a0da8edac35133
patinhooh <119509409+patinhooh@users.noreply.github.com>
2025-01-17 00:21:56 +0000

    feat: only accept valid location

--- monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/entities/ReadingLocation.java
+++ monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/entities/ReadingLocation.java
@@ -2,6 +2,9 @@ package pt.ipleiria.estg.dei.ei.dae.monitoring.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 import java.io.Serializable;
@@ -11,8 +14,12 @@ import java.time.Instant;
 @Table(name = "readings_location")
 public class ReadingLocation extends Reading implements Serializable {
 
-    @NotNull
+    @DecimalMin(value = "-90.0", message = "Latitude must be greater than or equal to -90")
+    @DecimalMax(value = "90.0", message = "Latitude must be less than or equal to 90")
     private Double latitude;
+
+    @DecimalMin(value = "-180.0", message = "Longitude must be greater than or equal to -180")
+    @DecimalMax(value = "180.0", message = "Longitude must be less than or equal to 180")
     private Double longitude;
 
     public ReadingLocation(Sensor sensor, Instant timestamp, Double latitude, Double longitude) {
