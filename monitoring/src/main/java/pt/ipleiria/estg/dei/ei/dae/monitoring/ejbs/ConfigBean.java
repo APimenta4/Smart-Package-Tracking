b1123557e9a9ce68264564c887189f9d08d5f84b
patinhooh <119509409+patinhooh@users.noreply.github.com>
2025-02-08 13:34:01 +0000

    After delivery: config for demo

--- monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/ejbs/ConfigBean.java
+++ monitoring/src/main/java/pt/ipleiria/estg/dei/ei/dae/monitoring/ejbs/ConfigBean.java
@@ -46,7 +46,7 @@ public class ConfigBean {
     private static final Logger logger = Logger.getLogger("ejbs.ConfigBean");
 
     @PostConstruct
-    public void populateDB(){
+    public void populateDB() {
         logger.info("Starting to populate DB");
 
         // Creating Users
@@ -76,7 +76,58 @@ public class ConfigBean {
             logger.warning("An error occurred while creating products: " + e.getMessage());
         }
 
-        logger.info("DB populated successfully");
+
+        logger.info("Creating order");
+        try {
+            orderBean.create("OrderTest1", "client");
+
+            volumeBean.create("VolumeTest1", "OrderTest1", PackageType.ISOTERMIC_GEOLOCATION);
+            sensorBean.create("A1", "VolumeTest1", SensorType.ACCELERATION);
+            sensorBean.create("L1", "VolumeTest1", SensorType.LOCATION);
+            sensorBean.create("T1", "VolumeTest1", SensorType.TEMPERATURE);
+            lineOfSaleBean.create("VolumeTest1", "P01", 10L);
+            lineOfSaleBean.create("VolumeTest1", "P02", 1L);
+
+            volumeBean.create("VolumeTest2", "OrderTest1", PackageType.FRAGILE);
+            sensorBean.create("A2", "VolumeTest2", SensorType.ACCELERATION);
+            sensorBean.create("L2", "VolumeTest2", SensorType.LOCATION);
+            sensorBean.create("T2", "VolumeTest2", SensorType.TEMPERATURE);
+            lineOfSaleBean.create("VolumeTest2", "P03", 1L);
+            lineOfSaleBean.create("VolumeTest2", "P04", 1L);
+
+
+            orderBean.create("OrderTest2", "client");
+
+            volumeBean.create("VolumeTest3", "OrderTest2", PackageType.ISOTERMIC_GEOLOCATION);
+            sensorBean.create("A3", "VolumeTest2", SensorType.ACCELERATION);
+            sensorBean.create("A31", "VolumeTest2", SensorType.ACCELERATION);
+            sensorBean.create("T3", "VolumeTest2", SensorType.TEMPERATURE);
+            sensorBean.create("L3", "VolumeTest2", SensorType.LOCATION);
+            lineOfSaleBean.create("VolumeTest2", "P05", 1L);
+            lineOfSaleBean.create("VolumeTest2", "P06", 1L);
+
+            volumeBean.create("VolumeTest4", "OrderTest2", PackageType.NONE);
+            sensorBean.create("A4", "VolumeTest2", SensorType.ACCELERATION);
+            sensorBean.create("T4", "VolumeTest4", SensorType.TEMPERATURE);
+            sensorBean.create("T41", "VolumeTest4", SensorType.TEMPERATURE);
+            sensorBean.create("L4", "VolumeTest4", SensorType.LOCATION);
+            sensorBean.create("L41", "VolumeTest4", SensorType.LOCATION);
+            lineOfSaleBean.create("VolumeTest4", "P07", 1L);
+            lineOfSaleBean.create("VolumeTest4", "P08", 1L);
+
+
+
+            readingTemperatureBean.create("S1", 24D);
+            readingLocationBean.create("S2", -50.61D, 165.97D);
+            readingAccelerationBean.create("S3", 1.5D);
+            readingTemperatureBean.create("S4", 24.2D);
+            readingLocationBean.create("S5", -50.61D, 165.97D);
+            readingAccelerationBean.create("S6", 1.5D);
+        } catch (Exception e) {
+            logger.warning("An error occurred while creating order: " + e.getMessage());
+        }
+        logger.info("Order created");
+        logger.info("DB populated");
     }
 }
 
